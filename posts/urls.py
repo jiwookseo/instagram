6120d04f3432c251c715f423e450b57b0e258ba4
Jiwook Seo <spjo21@naver.com>
2019-04-10 07:26:52 +0000

    Add index page, update urls path, views action

--- posts/urls.py
+++ posts/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.create, name='create'),
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
-
+from .models import Post
 
 def create(request):
     if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:create')
         return
     else:
         form = PostForm()
         return render(request, 'posts/create.html', {'form':form})
+
+def index(request):
+    posts = Post.objects.order_by('-id')
+    return render(request, 'posts/index.html', {'posts':posts})
