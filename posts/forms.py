e05be4a56c328c04cb6c349f87f2a903f5a28e18
Jiwook Seo <spjo21@naver.com>
2019-04-10 07:08:28 +0000

    Add posts/forms.py, PostForm

---
+++ posts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(label="content", widget=forms.Textarea(attrs={
+                    'rows': 7,
+                    'placeholder': '오늘은 무엇을 하셨나요'
+                }))
+    class Meta:
+        model = Post
+        fields = ['content']
