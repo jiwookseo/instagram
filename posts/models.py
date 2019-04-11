4c03e54e59ab432e096802336581609f6af9c233
Jiwook Seo <spjo21@naver.com>
2019-04-11 06:35:24 +0000

    Update Post model, add image field

--- posts/models.py
+++ posts/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     content = models.CharField(max_length=150)
+    image = models.ImageField(blank=True)
+    
+    def __str__(self):
+        return self.content
