9b75814b9259223d573581ced2f0aefcc8278dcd
Jiwook Seo <spjo21@naver.com>
2019-04-11 07:53:09 +0000

    Update post model, add user field

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.conf import settings
+
 
 class Post(models.Model):
     content = models.CharField(max_length=150)
     image = models.ImageField(blank=True, upload_to='postings/%Y%m%d')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     def __str__(self):
         return self.content
