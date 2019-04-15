83aff6bc58e1a92e9ad7109cf0b55600b8595ee6
Jiwook Seo <spjo21@naver.com>
2019-04-15 00:27:48 +0000

    Update post model, add liked_users M:N related field

--- posts/models.py
+++ posts/models.py
@@ -6,5 +6,6 @@ class Post(models.Model):
     content = models.CharField(max_length=150)
     image = models.ImageField(blank=True, upload_to='postings/%Y%m%d')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_posts", blank=True)
     def __str__(self):
         return self.content
