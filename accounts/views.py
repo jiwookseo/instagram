fda5e3236b20f7c763bc5cd929e34014ab4d3ae7
Jiwook Seo <spjo21@naver.com>
2019-04-15 04:53:30 +0000

    Add sign up feature

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import authenticate
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
 
 
@@ -20,7 +20,23 @@ def login(request):
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form':form})
     
+    
 def logout(request):
     auth_logout(request)
     messages.success(request, 'Logout success')
     return redirect('accounts:login')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            messages.success(request, 'Register Success, welcome {}'.format(user.username))
+            return redirect('posts:index')
+        else:
+            messages.success(request, 'Login fail, check your username or password')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/login.html', {'form':form})
