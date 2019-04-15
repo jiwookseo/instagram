c15a5da49da5cd225103f6a33c239b28668e55e2
Jiwook Seo <spjo21@naver.com>
2019-04-15 05:06:14 +0000

    Add user page feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
 
@@ -40,3 +40,9 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/login.html', {'form':form})
+    
+
+def people(request, username):
+    people = get_object_or_404(get_user_model(), username=username)
+    return render(request, 'accounts/people.html', {'people':people})
+    
--- instagram/urls.py
+++ instagram/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     path('accounts/', include('accounts.urls')),
+    path('<str:username>/', accounts_views.people, name="people"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
