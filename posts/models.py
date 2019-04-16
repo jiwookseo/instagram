5bd4ee40c707c501af9b1ad27cbb18938c6e923e
Jiwook Seo <spjo21@naver.com>
2019-04-16 00:27:40 +0000

    Add Comment model

--- posts/models.py
+++ posts/models.py
@@ -9,3 +9,11 @@ class Post(models.Model):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_posts", blank=True)
     def __str__(self):
         return self.content
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=150)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.content
