ffe3e3af45ee6b9255230671949947bae6f0ae79
Jiwook Seo <spjo21@naver.com>
2019-04-18 01:41:16 +0000

    Update Profile model, add profile image field

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, upload_to='profile/%Y%m%d', default='profile/default.jpg')
     description = models.TextField(blank=True)
     nickname = models.CharField(max_length=40, blank=True)
 
